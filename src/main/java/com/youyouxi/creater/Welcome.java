b59b8529d4f18ee84ba460bbf0e91d5e8b236f33
陈建斌 <86839224@qq.com>
2021-08-30 17:29:44 +0800

    创建工程

---
+++ src/main/java/com/youyouxi/creater/Welcome.java
@@ -0,0 +1,14 @@
+package com.youyouxi.creater;
+
+/**
+ * welcome
+ *
+ * @author youyouxi
+ */
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("你好！");
+    }
+
+}
